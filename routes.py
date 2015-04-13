cc0dbbf24ceab3fe9ec8cef0b04cba8738207745
Yasyf Mohamedali <yasyfm@gmail.com>
2015-04-13 01:01:19 -0400

    Keep track of rooms to cleanup better on disconnect.

--- routes.py
+++ routes.py
@@ -1,11 +1,12 @@
-from flask import render_template
+from flask import render_template, session
 from helpers.twitter import open_stream, close_stream, get_random_trending
 from flask.ext.socketio import join_room, leave_room
 from app import app, socketio
 
 @app.before_request
 def preprocess_request():
-  pass
+  if 'rooms' not in session:
+    session['rooms'] = {}
 
 @app.after_request
 def postprocess_request(response):
@@ -21,14 +22,23 @@ def translate_track(track):
   else:
     return 'everything'
 
+@socketio.on('disconnect')
+def handle_disconnect():
+  for room in session['rooms']:
+    close_stream(room)
+  session['rooms'] = {}
+
 @socketio.on('openStream')
 def handle_open_stream(data):
   track = translate_track(data['track'])
+  session['rooms'][track] = True
   join_room(track)
   open_stream(socketio, track)
 
 @socketio.on('closeStream')
 def handle_close_stream(data):
   track = translate_track(data['track'])
+  if track in session['rooms']:
+    del session['rooms'][track]
   leave_room(track)
   close_stream(track)
