5b3c32881afbcaf39085c90ef18a40b3746fd0c6
Yasyf Mohamedali <yasyfm@gmail.com>
2016-02-24 17:38:53 -0500

    Update Socket.io

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ if [ "$DEV" == "true" ]
 then
   python app.py
 else
-  gunicorn --worker-class eventlet -b "0.0.0.0:$PORT" app:app
+  gunicorn --worker-class eventlet -w 1 -b "0.0.0.0:$PORT" app:app
 fi
